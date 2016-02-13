517126b41ce31a89da941b658dd83bfd6d8d43ce
rojovivo27 <Titanium@I105-25.cucea.udg.mx>
2016-02-13 13:53:35 -0600

    Using camera and video Items

--- app/controllers/Captured.js
+++ app/controllers/Captured.js
@@ -1,1 +1,24 @@
 var args = arguments[0] || {};
+
+$.galleryButton.addEventListener('click', function() { 
+	var options = { 
+		success: function(e) { 
+		 // fired when user selects from gallery 
+		 // e.media == the image data
+		 $.imageArea.image = e.media;
+		}, 
+		cancel: function() { 
+		 // fired when user cancels 
+		 alert(":(");
+		}, 
+		error: function(error) { 
+		 // fired when there's an error 
+		 // error.code is a constant, like Titanium.Media.NO_CAMERA 
+		 alert("error \0/");
+		}, 
+		saveToPhotoGallery: false, 
+		mediaTypes: [Ti.Media.MEDIA_TYPE_PHOTO, Ti.Media.MEDIA_TYPE_VIDEO]
+	};
+	Ti.Media.showCamera(options); 
+});
+
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,20 +1,49 @@
 Ti.API.info('seeded:' + Ti.App.Properties.hasProperty("seeded"));
 
 if(!Ti.App.Properties.hasProperty("seeded")){
+	
+	
+	/*
 	var names = ["Jeff Haynie","Nolan Wright","Blain Hamon","Aaron Saunders","Aldo Hernandez"];
 	
 	for(var i=0, j=names.length ; i<j ; i++){
 		var criminal = Alloy.createModel("Fugitives", {name: names[i]});
 		criminal.save();
 	}
+	*/
 	
-	Ti.App.Properties.setBool('seeded',true);
+	var xhr = Titanium.Network.createHTTPClient();
+	xhr.onload = function () {
+		Ti.API.info('got data from the network: ' + this.responseText);
+		
+		var people = JSON.parse(this.responseText);
+		for(var i=0, j=people.length ; i<j ; i++){
+			var criminal = Alloy.createModel("Fugitives", {name: people[i].name});
+			criminal.save();
+			
+			console.info("name: " + people[i].name);
+		}
+		
+		Ti.App.Properties.setBool('seeded',true);
+		
+		Alloy.Collections.Fugitives.fetch();
+		
+	};
+	
+	xhr.open("GET","http://jsonplaceholder.typicode.com/users");
+	xhr.send();
+	
+	$.index.open();
+	
+} else {
+	
+	Alloy.Collections.Fugitives.fetch();
+	$.index.open();
+	
 }
 
-Alloy.Collections.Fugitives.fetch();
 
 
-$.index.open();
 
 
 //Pasos para traer datos de una BD local
